248d6dfbe7dc6de90731e7f91a564dd47dd7c6a1
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-07-04 08:36:45 +0200

    chore: correctly check the plugin setting

--- views/default/plugins/mmenu/settings.php
+++ views/default/plugins/mmenu/settings.php
@@ -9,7 +9,7 @@ echo elgg_view_field([
 	'#type' => 'checkbox',
 	'#label' => elgg_echo('mmenu:settings:topbar_to_site'),
 	'name' => 'params[topbar_to_site]',
-	'checked' => $plugin->topbar_to_site,
+	'checked' => (bool) $plugin->topbar_to_site,
 	'switch' => true,
 	'default' => 0,
 	'value' => 1,
