06ce7a198c9b7770ddf9604e60f10d6f37b942b3
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-12-14 15:32:19 +0100

    chore: updated translations

---
+++ languages/nl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-14 15:31
+ */
+
+return array (
+  'mmenu:settings:topbar_to_site' => 'Verplaats topbar menu items naar het sitemenu',
+);
