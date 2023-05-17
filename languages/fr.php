5747194ddad8e622c4419c9802ae400d29068b45
Jerôme Bakker <jeabakker@coldtrick.com>
2023-05-17 15:38:07 +0200

    chore: updated translations

--- languages/fr.php
+++ languages/fr.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * This file was created by Translation Editor v11.1
+ * On 2023-05-17 14:49
+ */
 
 return array (
   'mmenu:settings:topbar_to_site' => 'Déplacer les éléments de la barre supérieure vers le menu du site',
