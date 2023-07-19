4646c6419b031750cecdd50abcd9cc5d51de8a3a
Jerôme Bakker <jeabakker@coldtrick.com>
2023-07-19 10:12:05 +0200

    chore: output/url should have a discernible text

--- views/default/mmenu/topbar.php
+++ views/default/mmenu/topbar.php
@@ -5,10 +5,12 @@ $link = elgg_view_entity_url($site);
 
 $open = elgg_view_icon('chevron-right');
 $close = elgg_view_icon('chevron-left');
+
 $toggle = elgg_view('output/url', [
-	'class' => 'mmenu-toggle',
-	'href' => '#elgg-mmenu',
 	'text' => $open . $close,
+	'title' => elgg_echo('mmmenu:menu:topbar:toggle'),
+	'href' => '#elgg-mmenu',
+	'class' => 'mmenu-toggle',
 ]);
 
 echo elgg_format_element('div', ['class' => 'mmenu-topbar'], $link . $toggle);
